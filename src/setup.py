1fc334775d0a525553b45c5ccc8b89e669ab59cf
thrstnh <thrstn.hllbrnd@googlemail.com>
2012-06-14 20:24:54 +0200

    setup.py working
    PyQt4 does not use setuptools, so install it with your
    package manager and don't forget PyQt4.Phonon.

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,13 @@
-__author__="thrstnh"
-__date__ ="$03.02.2011 15:25:25$"
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from setuptools import setup,find_packages
+requires = ['mutagen', 'lyricwiki']
 
-setup (
-    name = 'pymp',
-    version = '0.1',
-    packages=find_packages(),
-    install_requires=['PyQt4',
-                      'sqlite3',
-                      'mutagen',
-                      'lyricwiki',
-                      'PyQt4.phonon'],
-    author = 'thrstnh',
-    author_email = 'thrstn.hllbrnd@gmail.com',
-    summary = 'yet another python music player',
-    url = 'https://github.com/thrstnh/pymp',
-    license = 'free for all')
+setup(name = 'pymp',
+      version = '0.1',
+      author = 'thrstnh',
+      author_email = 'thrstn.hllbrnd@gmail.com',
+      url = 'https://github.com/thrstnh/pymp',
+      packages=find_packages(),
+      install_requires=requires,
+      license = 'free for all')
